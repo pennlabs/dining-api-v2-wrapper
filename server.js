bf6577423999d5bc802d160f3d63f91e0ce67aca
Aaron Michaels <aaronmichaels@gmail.com>
2018-02-16 18:02:32 -0500

    Add cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const apirouter = require('./apirouter')
 const fs = require('fs')
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const schedule = require('node-schedule')
 
@@ -10,8 +11,10 @@ const schedule = require('node-schedule')
 //need to have the server seed itself on a schedule
 // require('./database/dbseed').seed()
 
+app.use(cors())
+
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/api', apirouter)
 
